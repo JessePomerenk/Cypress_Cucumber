f5254922d02f46291550bca361e23fd7261f2b36
Jesse Pomerenk <Jesse.pomerenk@gmail.com>
2025-01-21 15:12:57 -0800

    add eslint configs

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginCypress from 'eslint-plugin-cypress/flat'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  pluginCypress.configs.globals,
+  pluginCypress.configs.recommended
+];
